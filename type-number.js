25445ef499ce5bad7f2902d23f287653140edb8a
Teusocost <mateus.costaar@gmail.com>
2024-01-05 01:38:52 +0000

    first program ins JS to backend

---
+++ type-number.js
@@ -0,0 +1,15 @@
+const meuNumero =3;
+
+const firstNumber = 1;
+const secundNumber = 2;
+
+
+const result = firstNumber+secundNumber;
+
+console.log(result);
+
+// Nan -> not a number 
+
+const alura = "ALura";
+
+console.log(alura * firstNumber);
