ed456a67220664f21c0dca4ab230b05885a612c8
Teusocost <mateus.costaar@gmail.com>
2024-01-07 22:45:59 +0000

    test

---
+++ min_to_mais-string.js
@@ -0,0 +1,10 @@
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+
+const senha = "minhaSenha123"
+console.log(senha.length + "caracteres") // 13 caracteres
---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "olá, mundo";
+const texto2 = 'olá, mundo';
+const texto3 = "senhaSupersegura123";
+const StringdeNum = "123456";
+
+const citacao = "Opa 'Oi' ";
+const meuNome = "Mateus"
+
+//concatenação (+)
+console.log(citacao + meuNome);
+
+// template string ou template literal
+// template string
+const number = 10;
+const frase = ` e ai meu chapa você é ${number}`
+console.log(frase);
